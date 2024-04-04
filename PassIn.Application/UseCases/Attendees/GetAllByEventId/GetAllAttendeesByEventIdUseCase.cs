c8f3fa22cc6e966991023f039991aa1743581c8b
victor <josevictornascimento2016@gmail.com>
2024-04-03 21:18:35 -0300

    feat: checking attendees

---
+++ PassIn.Application/UseCases/Attendees/GetAllByEventId/GetAllAttendeesByEventIdUseCase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Attendees.GetAllByEventId
+{
+    public class GetAllAttendeesByEventIdUseCase
+    {
+        private readonly PassInDbContext _dbContext;
+
+        public GetAllAttendeesByEventIdUseCase()
+        {
+            _dbContext = new PassInDbContext();
+        }
+        public ResponseAllAttendeesJson Execute(Guid eventID)
+        {
+            var entity = _dbContext.Events.Include(ev => ev.Attendees).ThenInclude(attendee => attendee.CheckIn).FirstOrDefault(ev => ev.Id == eventID);
+
+            if (entity == null)
+                throw new NotFoundException("An event with id don't exist.");
+
+            return new ResponseAllAttendeesJson
+            {
+                Attendees = entity.Attendees.Select(attendee => new ResponseAttendeeJson
+                {
+                    Id = attendee.Id,
+                    Name = attendee.Name,
+                    Email = attendee.Email,
+                    CreatedAt = attendee.Created_at,
+                    CheckedInAt = attendee.CheckIn?.Created_at,
+                }).ToList(),
+            };
+        }
+    }
+}
---
+++ PassIn.Infrastructure/Entities/Attendee.cs
@@ -0,0 +1,12 @@
+﻿namespace PassIn.Infrastructure.Entities
+{
+    public class Attendee
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public Guid Event_id { get; set; }
+        public DateTime Created_at { get; set; }
+        public CheckIn? CheckIn { get; set; }
+    }
+}
